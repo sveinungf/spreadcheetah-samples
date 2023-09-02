069f7b26aaef7a6137a9e36cd5cfd6f9fb0dcbcb
sveinungf <ikonage@gmail.com>
2023-09-02 16:06:27 +0200

    Update SpreadCheetah to v1.11.0

--- SpreadCheetahSamples/DateTimeAndFormatting.cs
+++ SpreadCheetahSamples/DateTimeAndFormatting.cs
@@ -16,14 +16,14 @@ public static class DateTimeAndFormatting
 
         // Example of writing a DateTime with a custom number format. The date will be displayed as "18.10.2022".
         // Note that the format must be an Excel format code. More information here: https://support.microsoft.com/en-us/office/number-format-codes-5026bbd6-04bc-48cd-bf33-80f18b4eae68
-        var style1 = new Style { NumberFormat = "DD.MM.YYYY" };
+        var style1 = new Style { Format = NumberFormat.Custom("DD.MM.YYYY") };
         var style1Id = spreadsheet.AddStyle(style1);
         var cellA1 = new Cell(dateTime, style1Id);
         await spreadsheet.AddRowAsync(new[] { cellA1 });
 
         // Note that some characters have special meaning in the format codes. An example is the 'h' character, which signifies the hour.
         // Text can be escaped by enclosing them in double quotation marks. Here is an example of displaying the date as "18th":
-        var style2 = new Style { NumberFormat = "D\"th\"" };
+        var style2 = new Style { Format = NumberFormat.Custom("D\"th\"") };
         var style2Id = spreadsheet.AddStyle(style2);
         var cellA2 = new Cell(dateTime, style2Id);
         await spreadsheet.AddRowAsync(new[] { cellA2 });
@@ -31,7 +31,7 @@ public static class DateTimeAndFormatting
         // Also note that how some parts are displayed can depend on the regional/language setting of Excel.
         // This example will be shown as "October" in Excel when English (US) is the chosen language.
         // It will be shown as "oktober" in Excel when Norwegian is the chosen language.
-        var style3 = new Style { NumberFormat = "MMMM" };
+        var style3 = new Style { Format = NumberFormat.Custom("MMMM") };
         var style3Id = spreadsheet.AddStyle(style3);
         var cellA3 = new Cell(dateTime, style3Id);
         await spreadsheet.AddRowAsync(new[] { cellA3 });
