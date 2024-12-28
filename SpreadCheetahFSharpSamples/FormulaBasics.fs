a246ef4fddbb30fc9d0502cbb23915d9a9b40cf3
Rustam Sayfutdinov <rstm.sf@gmail.com>
2024-12-28 11:19:36 +0300

    Add F# Samples

---
+++ SpreadCheetahFSharpSamples/FormulaBasics.fs
@@ -0,0 +1,31 @@
+﻿module SpreadCheetahFSharpSamples.FormulaBasics
+
+open System.IO
+open SpreadCheetah
+
+let sample() = task {
+    use stream = File.Create("formula-basics.xlsx")
+    use! spreadsheet = Spreadsheet.CreateNewAsync(stream)
+
+    do! spreadsheet.StartWorksheetAsync("Sheet 1")
+
+    // Add numeric values to cells A1, A2, and A3.
+    do! spreadsheet.AddRowAsync([| Cell(10) |])
+    do! spreadsheet.AddRowAsync([| Cell(20) |])
+    do! spreadsheet.AddRowAsync([| Cell(30) |])
+
+    // Example of using the SUM formula to add the values above.
+    // NOTE: Formulas should NOT start with the `=` sign.
+    let formulaA4 = Formula("SUM(A1:A3)")
+    let cellA4 = Cell(formulaA4)
+    do! spreadsheet.AddRowAsync([| cellA4 |])
+
+    // Formula cells can optionally contain a cached value.
+    // Formula cells without a cached value will otherwise be calculated when displayed in Excel.
+    let formulaA5 = new Formula("AVERAGE(A1:A3)")
+    let cachedValue = 20
+    let cellA5 = Cell(formulaA5, cachedValue)
+    do! spreadsheet.AddRowAsync([| cellA5 |])
+
+    do! spreadsheet.FinishAsync()
+}
---
+++ SpreadCheetahFSharpSamples/Program.fs
@@ -0,0 +1,14 @@
+﻿module SpreadCheetahFSharpSamples.Program
+
+let main() = task {
+    do! DataValidations.sample()
+    do! DateTimeAndFormatting.sample()
+    do! DisposeAsync.sample()
+    do! FormulaBasics.sample()
+    do! PerformanceTips.sample()
+    do! StylingBasics.sample()
+    do! WriteToFile.sample()
+}
+
+main().GetAwaiter().GetResult()
+printfn "Done!"
